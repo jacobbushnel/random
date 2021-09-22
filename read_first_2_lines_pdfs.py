2308dd5e5de64fa32254c0f36a55ad06e9780bb3
Jake Bushnell <jacob.bushnell@gmail.com>
2021-09-21 20:15:22 -0400

    Update read_first_2_lines_pdfs.py
    Added ability to choose number of lines extracted.

--- read_first_2_lines_pdfs.py
+++ read_first_2_lines_pdfs.py
@@ -2,18 +2,21 @@ from pathlib import Path
 import os
 import PyPDF2
 
+##Used to extract text from all PDFs in a given folder and extract first N number of lines and save results to pdf_extracted.txt in users home directory
+##It also extracts author and title and saves with extracted lines in pdf_extracted.txt
 print("This will check every file in the folder to see if it is a pdf\nif it is then it will open the file, copy the first 2 lines\nand put them in a text file in your home dir pdf_first_2_lines.txt\nNOTE: this does not check any nested folders.")
 
 given_folder = input("Folder path from home dir (i.e. desktop, documents\pdfFolder: ")
+num_of_lines = input("How many lines of text to extract: ")
 folder = str(Path.home()) + '\\'+given_folder
 #folder = 'c:\\users\\jake\\documents'
-results_file = str(Path.home())+'\pdf_first_2_lines.txt'
+results_file = str(Path.home())+'\pdf_extracted.txt'
 try:
-    first_2_lines = open(str(Path.home())+'\pdf_first_2_lines.txt', 'x').close()
+    first_2_lines = open(str(Path.home())+'\pdf_extracted.txt', 'x').close()
 except FileExistsError:
     input("File already exists in home directory, press enter to overwrite.")
     os.remove(str(Path.home())+'\pdf_first_2_lines.txt')
-    first_2_lines = open(str(Path.home())+'\pdf_first_2_lines.txt', 'x').close()
+    first_2_lines = open(str(Path.home())+'\pdf_extracted.txt', 'x').close()
     
 print("File created")
 for file in os.scandir(folder):
@@ -22,7 +25,6 @@ for file in os.scandir(folder):
         readpdf = PyPDF2.PdfFileReader(pdf)
         information = readpdf.getDocumentInfo()
         page = readpdf.getPage(0)
-        #text = firstPage.extractText()
         text = page.extractText()
         head = f"""
                 Author: {information.author}
@@ -35,7 +37,7 @@ for file in os.scandir(folder):
                 f.write('first page of pdf contains unsupported characters (e.g. nontext)')
             f.close
         with open('tmp.txt', 'r') as f:   
-            lines = f.readlines(2)
+            lines = f.readlines(num_of_lines)
             with open(results_file, 'a') as f2l:
                 results = (str(head) +'\r\n'+str(lines)+'\r\n--------------')
                 f2l.write(results+'\r\n')
